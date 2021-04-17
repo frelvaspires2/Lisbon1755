6f72fbd351b92fe2aa0d9ada35187aea071d8f07
Miguel <miguel.angelo95@gmail.com>
2021-04-17 18:54:28 +0100

    animation polishment.
    3/8

---
+++ Lisbon1755/Assets/Scripts/Animations/PlayAnim.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayAnim : MonoBehaviour
+{
+    // Player's gameobject.
+    [SerializeField]
+    private GameObject player;
+
+    // Animation controller.
+    [SerializeField]
+    private Animator animController;
+
+    // Check if the animation was already played.
+    private bool isPlayed;
+
+    /// <summary>
+    /// To be played in the first frame of the game.
+    /// Initialize variables.
+    /// </summary>
+    private void Start()
+    {
+        isPlayed = false;
+    }
+
+    /// <summary>
+    /// Detect trigger when enters.
+    /// </summary>
+    /// <param name="other"> The object collider.</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        // If triggers player and the animation hasn't already played.
+        if(other == player && !isPlayed)
+        {
+            // Can't play next time.
+            isPlayed = true;
+            // Play the animation (insert the animation name).
+            animController.Play("");
+        }
+    }
+}
