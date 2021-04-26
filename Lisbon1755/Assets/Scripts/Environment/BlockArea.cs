c4f03ecc572b965abb2ea222d40b7fe8bd9a5f76
Francisco <frelvaspires@gmail.com>
2021-04-26 18:52:53 +0100

    Some improvements.

---
+++ Lisbon1755/Assets/Scripts/Environment/BlockArea.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BlockArea : MonoBehaviour
+{
+    [SerializeField]
+    private Collider blocker;
+
+    [SerializeField]
+    private PlayAnim playAnim;
+
+    private void Start()
+    {
+        blocker.isTrigger = true;
+    }
+
+    private void Update()
+    {
+        if (playAnim.IsDone)
+        {
+            Block();
+        }
+    }
+
+    private void Block()
+    {
+        blocker.isTrigger = false;
+    }
+}
+
