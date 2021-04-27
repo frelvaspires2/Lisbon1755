3cfd1a99ce15cd55f106aa829809b95c2b0c2999
Francisco <frelvaspires@gmail.com>
2021-04-27 12:42:52 +0100

    Wake up event polished.

--- Lisbon1755/Assets/Scripts/Events/Specific/EndAnim.cs
+++ Lisbon1755/Assets/Scripts/Events/Specific/EndAnim.cs
@@ -1,24 +1,44 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Script to end animation for wake up event.
+/// </summary>
 public class EndAnim : MonoBehaviour
 {
+    /// <summary>
+    /// Access the CameraShake script.
+    /// </summary>
     [SerializeField]
     private CameraShake cameraShake;
 
+    /// <summary>
+    /// Check whether the animation has finished.
+    /// </summary>
     [SerializeField]
     private bool isAnimationFinished;
 
+    /// <summary>
+    /// Gets whether the animation has finished.
+    /// </summary>
     public bool IsAnimationFinished { get => isAnimationFinished; }
 
+    /// <summary>
+    /// Access the damage zone script.
+    /// </summary>
     [SerializeField]
     private GameObject dmgZone;
 
+    /// <summary>
+    /// Access the block zone script.
+    /// </summary>
     [SerializeField]
     private GameObject blockZone;
 
-
+    /// <summary>
+    /// To be played in the first frame of the game.
+    /// Setup variables.
+    /// </summary>
     private void Start()
     {
         isAnimationFinished = false;
@@ -26,14 +46,21 @@ public class EndAnim : MonoBehaviour
         blockZone.SetActive(false);
     }
 
+    /// <summary>
+    /// Stop the animation.
+    /// To be called in the animation frame as event.
+    /// </summary>
     public void StopAnim()
     {
-        //cameraShake.CanShake = false;
         isAnimationFinished = true;
         dmgZone.SetActive(true);
         StartCoroutine(StopDMG());
     }
 
+    /// <summary>
+    /// Stop the damage.
+    /// </summary>
+    /// <returns> Wait for seconds.</returns>
     private IEnumerator StopDMG()
     {
         WaitForSeconds wfs = new WaitForSeconds(1);
