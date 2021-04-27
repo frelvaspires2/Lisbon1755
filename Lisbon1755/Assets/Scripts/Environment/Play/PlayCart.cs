dac68d5a0235e667e61b77d8bf89da47205e3161
Francisco <frelvaspires@gmail.com>
2021-04-27 15:48:48 +0100

    Animation trigger added in cart and another building.

---
+++ Lisbon1755/Assets/Scripts/Environment/Play/PlayCart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCart : MonoBehaviour
+{
+    [SerializeField]
+    private Animator cartAnimator;
+
+    [SerializeField]
+    private GameObject player;
+
+
+    private void Start()
+    {
+        cartAnimator.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            cartAnimator.enabled = true;
+        }
+    }
+}
