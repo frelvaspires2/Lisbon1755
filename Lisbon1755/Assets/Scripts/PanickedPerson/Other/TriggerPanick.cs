4260ca42d4d9d8d9929f5809da992fe985c4a796
Francisco <frelvaspires@gmail.com>
2021-04-29 17:59:15 +0100

    Basic panicked crowd added in level 1.

---
+++ Lisbon1755/Assets/Scripts/PanickedPerson/Other/TriggerPanick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class TriggerPanick : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private GameObject spawn;
+
+    private bool isSpawn;
+
+    private void Start()
+    {
+        isSpawn = false;
+        spawn.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            if(!isSpawn)
+            {
+                spawn.SetActive(true);
+                isSpawn = true;
+            }
+        }
+    }
+}
