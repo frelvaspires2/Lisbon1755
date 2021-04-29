542e91e89c61b4b2e1189de5ef72cdc3ed15a9ad
Miguel <miguel.angelo95@gmail.com>
2021-04-29 22:19:14 +0100

    Got Hit Screen script added.

---
+++ Lisbon1755/Assets/Scripts/UI/UI_GotHitScreen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_GotHitScreen : MonoBehaviour
+{
+    public GameObject m_GotHitScreen;
+    public GameObject MyObject;
+
+    void Start()
+    {
+        
+    }
+
+    void gotHurt()
+    {
+        var color = m_GotHitScreen.GetComponent<Image>().color;
+        color.a = 0.8f;
+
+        m_GotHitScreen.GetComponent<Image>().color = color;
+    }
+
+    void Update()
+    {
+        if (MyObject.GetComponent<GameData>().IsInjured == true)
+            {
+                gotHurt();
+            }
+
+        if (m_GotHitScreen != null)
+        {
+            if (m_GotHitScreen.GetComponent<Image>().color.a > 0)
+            {
+                var color = m_GotHitScreen.GetComponent<Image>().color;
+                color.a -= 0.01f;
+                m_GotHitScreen.GetComponent<Image>().color = color;
+            }
+        }
+    }
+}
+ 
