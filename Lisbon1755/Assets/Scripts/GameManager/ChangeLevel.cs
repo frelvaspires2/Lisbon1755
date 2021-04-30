923cfb586b362d153ad7b8f58864c6f0f76ee6a6
Francisco <frelvaspires@gmail.com>
2021-04-30 12:38:16 +0100

    Some improvements added.

---
+++ Lisbon1755/Assets/Scripts/GameManager/ChangeLevel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    private int scene;
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private bool isEnd;
+
+    private void Start()
+    {
+        scene = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            if (!isEnd)
+            {
+                SceneManager.LoadScene(scene + 1);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
