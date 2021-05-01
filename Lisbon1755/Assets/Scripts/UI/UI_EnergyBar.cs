1c4df11579b9ffadbf34282814dc3fb6707e74f1
Francisco <frelvaspires@gmail.com>
2021-05-01 15:43:07 +0100

    Improve Miguel's UI energy script.

--- Lisbon1755/Assets/Scripts/UI/UI_EnergyBar.cs
+++ Lisbon1755/Assets/Scripts/UI/UI_EnergyBar.cs
@@ -7,28 +7,10 @@ using UnityEngine.UI;
 public class UI_EnergyBar : MonoBehaviour
 {
     /// <summary>
-    /// Access the energy bar.
+    /// Access the slider UI.
     /// </summary>
     [SerializeField]
     public Slider energyBar;
-
-    /// <summary>
-    /// Set the max energy value.
-    /// </summary>
-    [SerializeField]
-    private float maxEnergy;
-
-    /// <summary>
-    /// Check what is the current energy value.
-    /// </summary>
-    [SerializeField]
-    private float currentEnergy;
-
-    /// <summary>
-    /// Access the slider.
-    /// </summary>
-    [SerializeField]
-    private Slider slider;
 
     /// <summary>
     /// Access the GameData script.
@@ -36,36 +18,6 @@ public class UI_EnergyBar : MonoBehaviour
     [SerializeField]
     private GameData gameData;
 
-    /// <summary>
-    /// Access the PlayerStats scriptable object.
-    /// </summary>
-    [SerializeField]
-    private PlayerStats playerStats;
-
-    /// <summary>
-    /// To be played in the first frame.
-    /// Setup variables.
-    /// </summary>
-    private void Start()
-    {
-        maxEnergy = playerStats.Energy;
-
-        currentEnergy = maxEnergy;
-
-        energyBar.maxValue = maxEnergy;
-
-        energyBar.value = maxEnergy;
-    }
-
-    /// <summary>
-    /// Refill the slider.
-    /// </summary>
-    private void SliderEnergy()
-    {
-        currentEnergy = gameData.Energy;
-
-        energyBar.value = currentEnergy;
-    }
 
     /// <summary>
     /// To be played in every frame.
@@ -73,9 +25,6 @@ public class UI_EnergyBar : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        if(gameData.Energy < playerStats.Energy)
-        {
-            SliderEnergy();
-        }
+        energyBar.value = gameData.Energy;
     }
 }
