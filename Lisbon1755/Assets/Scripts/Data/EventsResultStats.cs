67419dd38a7080e30d7f890451359e065c7739d4
Francisco <frelvaspires@gmail.com>
2021-05-02 15:23:15 +0100

    Final score (wip)

---
+++ Lisbon1755/Assets/Scripts/Data/EventsResultStats.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "EventsResultStats")]
+public class EventsResultStats : ScriptableObject
+{
+    [SerializeField]
+    private EventResult[] eventsResult;
+   
+    public EventResult[] EventsResult
+    {
+        get => eventsResult;
+        set => eventsResult = value;
+    }
+}
