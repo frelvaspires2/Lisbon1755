14456d5ce541a19ba26f46bfc72a61daac77d7da
Francisco <frelvaspires@gmail.com>
2021-05-11 15:50:36 +0100

    Code polish.

--- Lisbon1755/Assets/Scripts/PanickedPerson/Animations/SetPanickModel.cs
+++ Lisbon1755/Assets/Scripts/PanickedPerson/Animations/SetPanickModel.cs
@@ -1,122 +1,142 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// Setup the character for the Panicked person.
+/// </summary>
 public class SetPanickModel : MonoBehaviour
 {
+    /// <summary>
+    /// Access the character types enum.
+    /// </summary>
     [SerializeField]
     private PanickModelTypes panickModelTypes;
 
+    /// <summary>
+    /// Access the prototype characters.
+    /// </summary>
     [SerializeField]
     private GameObject[] prototype;
 
+    /// <summary>
+    /// Access the character1 characters.
+    /// </summary>
     [SerializeField]
     private GameObject[] character1;
 
+    /// <summary>
+    /// Access the character2 characters.
+    /// </summary>
     [SerializeField]
     private GameObject[] character2;
 
+    /// <summary>
+    /// Access the character3 characters.
+    /// </summary>
     [SerializeField]
     private GameObject[] character3;
 
+    /// <summary>
+    /// Access the character4 characters.
+    /// </summary>
     [SerializeField]
     private GameObject[] character4;
 
+    /// <summary>
+    /// Access the character5 characters.
+    /// </summary>
     [SerializeField]
     private GameObject[] character5;
 
+    /// <summary>
+    /// Access the character6 characters.
+    /// </summary>
     [SerializeField]
     private GameObject[] character6;
 
+    /// <summary>
+    /// To be played in the first frame.
+    /// Initialize and set up the characters.
+    /// </summary>
     private void Start()
     {
-        InitializeModels();
-        SetModel();
+        InitializeCharacters();
+        SetCharacter();
     }
 
-    private void InitializeModels()
+    /// <summary>
+    /// Initialize the characters.
+    /// </summary>
+    private void InitializeCharacters()
     {
-        for (int i = 0; i < prototype.Length; i++)
-        {
-            prototype[i].SetActive(false);
-        }
+        TurnOff(ref prototype);
 
-        for (int i = 0; i < character1.Length; i++)
-        {
-            character1[i].SetActive(false);
-        }
+        TurnOff(ref character1);
 
-        for (int i = 0; i < character2.Length; i++)
-        {
-            character2[i].SetActive(false);
-        }
+        TurnOff(ref character2);
 
-        for (int i = 0; i < character3.Length; i++)
-        {
-            character3[i].SetActive(false);
-        }
+        TurnOff(ref character3);
 
-        for (int i = 0; i < character4.Length; i++)
-        {
-            character4[i].SetActive(false);
-        }
+        TurnOff(ref character4);
 
-        for (int i = 0; i < character5.Length; i++)
-        {
-            character5[i].SetActive(false);
-        }
+        TurnOff(ref character5);
 
-        for (int i = 0; i < character6.Length; i++)
-        {
-            character6[i].SetActive(false);
-        }
+        TurnOff(ref character6);
     }
 
-    private void SetModel()
+    /// <summary>
+    /// Set up the characters.
+    /// </summary>
+    private void SetCharacter()
     {
         switch(panickModelTypes)
         {
             case PanickModelTypes.Character1:
-                for (int i = 0; i < character1.Length; i++)
-                {
-                    character1[i].SetActive(true);
-                }
+                TurnOn(ref character1);
                 break;
 
             case PanickModelTypes.Character2:
-                for (int i = 0; i < character2.Length; i++)
-                {
-                    character2[i].SetActive(true);
-                }
+                TurnOn(ref character2);
                 break;
 
             case PanickModelTypes.Character3:
-                for (int i = 0; i < character3.Length; i++)
-                {
-                    character3[i].SetActive(true);
-                }
+                TurnOn(ref character3);
                 break;
 
             case PanickModelTypes.Character4:
-                for (int i = 0; i < character4.Length; i++)
-                {
-                    character4[i].SetActive(true);
-                }
+                TurnOn(ref character4);
                 break;
 
             case PanickModelTypes.Character5:
-                for (int i = 0; i < character5.Length; i++)
-                {
-                    character5[i].SetActive(true);
-                }
+                TurnOn(ref character5);
                 break;
 
             case PanickModelTypes.Character6:
-                for (int i = 0; i < character6.Length; i++)
-                {
-                    character6[i].SetActive(true);
-                }
+                TurnOn(ref character6);
                 break;
+        }
+    }
+
+    /// <summary>
+    /// Turn off the target character.
+    /// </summary>
+    /// <param name="character"> Chosen character.</param>
+    private void TurnOff(ref GameObject[] character)
+    {
+        for(int i = 0; i < character.Length; i++)
+        {
+            character[i].SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Turn on the target character.
+    /// </summary>
+    /// <param name="character"> Chosen target.</param>
+    private void TurnOn(ref GameObject[] character)
+    {
+        for (int i = 0; i < character.Length; i++)
+        {
+            character[i].SetActive(true);
         }
     }
 }
