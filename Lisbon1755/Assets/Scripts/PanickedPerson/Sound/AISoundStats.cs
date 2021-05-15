b4cb5f67d9bafe30d4cd912dccd88d5c76473fcf
Francisco <frelvaspires@gmail.com>
2021-05-15 16:10:07 +0100

    Sound scripts added.

---
+++ Lisbon1755/Assets/Scripts/PanickedPerson/Sound/AISoundStats.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+
+/// <summary>
+/// Sound stats for each AI sound.
+/// </summary>
+[Serializable]
+public struct AISoundStats 
+{
+    /// <summary>
+    /// Access the sound gameobject.
+    /// </summary>
+    [SerializeField]
+    private GameObject sound;
+
+    /// <summary>
+    /// Gets the sound gameobject.
+    /// </summary>
+    public GameObject Sound { get => sound; }
+
+    /// <summary>
+    /// Access the sound types.
+    /// </summary>
+    [SerializeField]
+    private AISoundTypes aiSoundTypes;
+
+    /// <summary>
+    /// Gets the sound types.
+    /// </summary>
+    public AISoundTypes GetAISoundType { get => aiSoundTypes; }
+}
