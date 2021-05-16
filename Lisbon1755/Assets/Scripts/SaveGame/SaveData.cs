ec32e50c90f398741e20e1ac3807043b4232bace
Francisco <frelvaspires@gmail.com>
2021-05-16 13:17:42 +0100

    Save and load game (wip)

---
+++ Lisbon1755/Assets/Scripts/SaveGame/SaveData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct SaveData 
+{
+    public int Level { get; }
+
+    public bool IsFinished { get; }
+
+    public int PeopleSaved { get; }
+
+    public SaveData(int level, bool isFinished, int peopleSaved)
+    {
+        Level = level;
+        IsFinished = isFinished;
+        PeopleSaved = peopleSaved;
+    }
+}
---
+++ Lisbon1755/Assets/Scripts/SaveGame/SaveGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class SaveGame : MonoBehaviour
+{
+    [SerializeField]
+    private GameStats gameStats;
+
+    private string path; //= Application.persistentDataPath + "/save.csv";
+
+    private void Awake()
+    {
+        path = "C:/Users/Francisco/Documents/GitHub/Lisbon1755/save.csv";
+        CreateFile();
+    }
+
+    private void CreateFile()
+    {
+        using(StreamWriter sw = File.CreateText(path))
+        {
+            sw.WriteLine("Level, IsFinished?, PeopleSaved");
+            sw.WriteLine("1, true, 3");
+            sw.WriteLine("2, false, 0");
+        }
+    }
+}
