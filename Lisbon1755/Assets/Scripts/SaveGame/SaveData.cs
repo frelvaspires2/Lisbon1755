bd667c9a801926f449e161d4b317c5bb03ba187c
Francisco <frelvaspires@gmail.com>
2021-05-16 14:04:24 +0100

    Save and load game (wip)

--- Lisbon1755/Assets/Scripts/SaveGame/SaveData.cs
+++ Lisbon1755/Assets/Scripts/SaveGame/SaveData.cs
@@ -1,15 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public struct SaveData 
+﻿
+/// <summary>
+/// Save game stats.
+/// </summary>
+public struct SaveData
 {
+    /// <summary>
+    /// Level number.
+    /// </summary>
     public int Level { get; }
 
+    /// <summary>
+    /// Gets whether the level is finished.
+    /// </summary>
     public bool IsFinished { get; }
 
+    /// <summary>
+    /// Number of people saved.
+    /// </summary>
     public int PeopleSaved { get; }
 
+    /// <summary>
+    /// Initialize the properties.
+    /// </summary>
+    /// <param name="level"> Level number. </param>
+    /// <param name="isFinished"> Checks whether the level is finished. </param>
+    /// <param name="peopleSaved"> Number of people saved. </param>
     public SaveData(int level, bool isFinished, int peopleSaved)
     {
         Level = level;
--- Lisbon1755/Assets/Scripts/SaveGame/SaveGame.cs
+++ Lisbon1755/Assets/Scripts/SaveGame/SaveGame.cs
@@ -1,29 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using System;
 using System.IO;
 
+/// <summary>
+/// Save the game.
+/// </summary>
 public class SaveGame : MonoBehaviour
 {
+    /// <summary>
+    /// Access the GameStats scriptableobject.
+    /// </summary>
     [SerializeField]
     private GameStats gameStats;
 
+    /// <summary>
+    /// Set the file path.
+    /// </summary>
     private string path; //= Application.persistentDataPath + "/save.csv";
 
+    /// <summary>
+    /// To be played before the game begins.
+    /// Set the file path.
+    /// </summary>
     private void Awake()
     {
         path = "C:/Users/Francisco/Documents/GitHub/Lisbon1755/save.csv";
-        CreateFile();
     }
 
-    private void CreateFile()
+    /// <summary>
+    /// Save the game by creating/overwrite a save file.
+    /// </summary>
+    /// <param name="info"> The game stats to be saved.</param>
+    public void SaveTheGame(Dictionary<int, SaveData> info)
     {
         using(StreamWriter sw = File.CreateText(path))
         {
             sw.WriteLine("Level, IsFinished?, PeopleSaved");
-            sw.WriteLine("1, true, 3");
-            sw.WriteLine("2, false, 0");
+
+            for(int i = 0; i < info.Count; i++)
+            {
+                sw.WriteLine($"{info[i].Level}, {info[i].IsFinished}, {info[i].PeopleSaved}");
+            }
         }
     }
 }
