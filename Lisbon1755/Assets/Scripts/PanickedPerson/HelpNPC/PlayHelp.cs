af54fd63bdcd7cce77c51165c439ac03daa9dcae
Francisco <frelvaspires@gmail.com>
2021-05-22 15:47:33 +0100

    Code commented.

--- Lisbon1755/Assets/Scripts/PanickedPerson/HelpNPC/PlayHelp.cs
+++ Lisbon1755/Assets/Scripts/PanickedPerson/HelpNPC/PlayHelp.cs
@@ -1,33 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// Help the agent when he's in the wounded state.
+/// </summary>
 public class PlayHelp : MonoBehaviour
 {
+    /// <summary>
+    /// Access the player gameobject.
+    /// </summary>
     [SerializeField]
     private GameObject player;
 
+    /// <summary>
+    /// Access the PanickedController script.
+    /// </summary>
     [SerializeField]
     private PanickedController panickedController;
 
+    /// <summary>
+    /// Access the PanickedStats scriptableobject.
+    /// </summary>
     [SerializeField]
     private PanickedStats panickedStats;
 
+    /// <summary>
+    /// Access the Transform of the helping position.
+    /// </summary>
     [SerializeField]
     private Transform setPosition;
 
+    /// <summary>
+    /// Checks whether the agent is wounded.
+    /// </summary>
     [SerializeField]
     private bool isWounded;
 
+    /// <summary>
+    /// Checks whether the player is inside the helping area.
+    /// </summary>
     [SerializeField]
     private bool isInside;
 
+    /// <summary>
+    /// Set how many clicks are needed to help the agent.
+    /// </summary>
     [SerializeField]
     private float setClicks;
 
+    /// <summary>
+    /// Count the number of clicks.
+    /// </summary>
     [SerializeField]
     private float clickCount;
 
+    /// <summary>
+    /// To be played in the first frame of the game.
+    /// Initialize variables.
+    /// </summary>
     private void Start()
     {
         isInside = false;
@@ -35,12 +64,18 @@ public class PlayHelp : MonoBehaviour
         clickCount = 0;
     }
 
+    /// <summary>
+    /// To be played in every frame of the game.
+    /// </summary>
     private void Update()
     {
         CheckState();
         Help();
     }
 
+    /// <summary>
+    /// Checks if the opponent is in wounded state.
+    /// </summary>
     private void CheckState()
     {
         if(panickedController.GetStates == PanickedState.WoundedInTheGround)
@@ -55,19 +90,19 @@ public class PlayHelp : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Help the agent.
+    /// </summary>
     private void Help()
     {
         if(isInside)
         {
             if(Input.GetButton("MouseClick"))
             {
-                //Debug.Log("Helping NPC");
                 player.GetComponent<CharacterController>().enabled = false;
                 player.transform.position = setPosition.position;
                 player.transform.rotation = setPosition.rotation;
                 player.GetComponent<CharacterController>().enabled = true;
-
-                //panickedController.Health += 1 * Time.deltaTime;
 
                 if(clickCount < setClicks)
                 {
@@ -82,6 +117,10 @@ public class PlayHelp : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Checks whether the player is close to the agent.
+    /// </summary>
+    /// <param name="other"> Collider.</param>
     private void OnTriggerStay(Collider other)
     {
         if(other.gameObject == player && isWounded)
@@ -90,6 +129,10 @@ public class PlayHelp : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Checks whether the player is close to the agent.
+    /// </summary>
+    /// <param name="other"> Collider.</param>
     private void OnTriggerExit(Collider other)
     {
         if(other.gameObject == player && isWounded)
