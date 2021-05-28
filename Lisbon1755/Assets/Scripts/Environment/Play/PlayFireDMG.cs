d141c1930376b330eb91f24ca81d44e0c5070d1d
Francisco <frelvaspires@gmail.com>
2021-05-28 14:26:28 +0100

    Some fixing done.

---
+++ Lisbon1755/Assets/Scripts/Environment/Play/PlayFireDMG.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Give dmg to the player or NPC when they are inside fire.
+/// </summary>
+public class PlayFireDMG : MonoBehaviour
+{
+    /// <summary>
+    /// Access the player gameobject.
+    /// </summary>
+    [SerializeField]
+    private GameObject player;
+
+    /// <summary>
+    /// Set the damage points.
+    /// </summary>
+    [SerializeField]
+    private float dmg;
+
+    /// <summary>
+    /// Check if something entered on the trigger.
+    /// </summary>
+    /// <param name="other"> Collider</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        // If it's the player, then decrement health.
+        if (other.gameObject == player)
+        {
+            player.GetComponent<PlayerHealth>().Health -= dmg;
+            player.GetComponent<UI_GotHitScreen>().GotHit();
+        }
+        // If it's an NPC, then decrement health.
+        if (other.gameObject.tag == "NPC")
+        {
+            other.GetComponent<PanickedController>().Health -= dmg;
+            other.GetComponent<AISoundController>().GotHitSound();
+        }
+    }
+}
