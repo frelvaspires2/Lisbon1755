d3dd8b7684ad1346eca4bb9c1e76346db63088d5
Miguel <miguel.angelo95@gmail.com>
2021-06-07 14:26:06 +0100

    Main Menu 75%
    Script On Hover

---
+++ Lisbon1755/Assets/Scripts/UI/ChangeOnHover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeOnHover : MonoBehaviour
+{
+    public Material [] material;
+    Renderer rend;
+
+    public Transform sizzle;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+        sizzle.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+
+    void OnMouseOver()
+    {
+       rend.sharedMaterial = material[1];
+       sizzle.GetComponent<ParticleSystem>().enableEmission = true;
+       StartCoroutine(stopSizzles());
+    }
+
+    IEnumerator stopSizzles()
+    {
+        yield return new WaitForSeconds(1f);
+        sizzle.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+    void OnMouseExit()
+    {
+        rend.sharedMaterial = material[0];
+    }
+}
