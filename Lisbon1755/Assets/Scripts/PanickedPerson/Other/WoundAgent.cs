66894c2e259a3628b0818b3a069d0ea9afc7d3f5
Francisco <frelvaspires@gmail.com>
2021-06-08 12:37:13 +0100

    Some wounded agents added to the game.

---
+++ Lisbon1755/Assets/Scripts/PanickedPerson/Other/WoundAgent.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoundAgent : MonoBehaviour
+{
+    [SerializeField]
+    private WoundAgentStats[] selectedAgent;
+
+    [SerializeField]
+    private float dmg;
+
+    private void Start()
+    {
+        for(int i = 0; i < selectedAgent.Length; i++)
+        {
+            selectedAgent[i].IsDMG = false;
+        }
+    }
+
+    private void Update()
+    {
+        GiveDamage();
+    }
+
+    private bool CheckIfActive(GameObject agent)
+    {
+        if (agent.activeSelf)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void GiveDamage()
+    {
+        for (int i = 0; i < selectedAgent.Length; i++)
+        {
+            if (CheckIfActive(selectedAgent[i].SelectedAgent))
+            {
+                if (!selectedAgent[i].IsDMG)
+                {
+                    selectedAgent[i].SelectedAgent.GetComponent<PanickedController>().Health -= dmg;
+                    selectedAgent[i].IsDMG = true;
+                }
+            }
+        }
+    }
+}
---
+++ Lisbon1755/Assets/Scripts/PanickedPerson/Other/WoundAgentStats.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public struct WoundAgentStats 
+{
+    [SerializeField]
+    private GameObject selectedAgent;
+
+    public GameObject SelectedAgent { get => selectedAgent; }
+
+    [SerializeField]
+    private bool isDMG;
+
+    public bool IsDMG { get => isDMG; set => isDMG = value; }
+}
