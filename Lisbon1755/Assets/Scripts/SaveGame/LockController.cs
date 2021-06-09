ca0105889ef195fb6de4813fa4115bea1528af98
Francisco <frelvaspires@gmail.com>
2021-06-09 14:11:20 +0100

    The main menu now verifies if level 2 was unlocked.

---
+++ Lisbon1755/Assets/Scripts/SaveGame/LockController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] lockUI;
+
+    [SerializeField]
+    private LoadGame loadGame;
+
+    private void Start()
+    {
+        if (loadGame.CheckIfLevel2IsUnlocked())
+        {
+            lockUI[0].SetActive(false);
+        }
+        else
+        {
+            lockUI[0].SetActive(true);
+        }
+    }
+}
