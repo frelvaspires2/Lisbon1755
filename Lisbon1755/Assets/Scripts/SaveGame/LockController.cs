50b84f890d030fde9271f73d8b93975100d70ac3
Francisco <frelvaspires@gmail.com>
2021-06-09 14:45:00 +0100

    The data from level 1 now loads.

--- Lisbon1755/Assets/Scripts/SaveGame/LockController.cs
+++ Lisbon1755/Assets/Scripts/SaveGame/LockController.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Control the lock in the main menu.
+/// </summary>
 public class LockController : MonoBehaviour
 {
+    /// <summary>
+    /// Lock UI.
+    /// </summary>
     [SerializeField]
     private GameObject[] lockUI;
 
+    /// <summary>
+    /// Access the load game script.
+    /// </summary>
     [SerializeField]
     private LoadGame loadGame;
 
+    /// <summary>
+    /// To be played in the first frame of the game.
+    /// </summary>
     private void Start()
     {
         if (loadGame.CheckIfLevel2IsUnlocked())
@@ -23,14 +33,21 @@ public class LockController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Check if level 2 is unlocked. If so, load the score from level 1.
+    /// </summary>
     public void PlayLevel2()
     {
         if (!lockUI[0].activeSelf)
         {
+            loadGame.LoadTheGame();
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
         }
     }
 
+    /// <summary>
+    /// Play level 1.
+    /// </summary>
     public void PlayLevel1()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
